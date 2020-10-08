46fd485cddf036cfc97b646e74f08edf3e3c5c87
Pierre Roy <lrp.roy@gmail.com>
2020-10-08 20:43:56 +0200

    CCTV shader

--- Assets/Scripts/UI/SecurityCameraOverlayUI.cs
+++ Assets/Scripts/UI/SecurityCameraOverlayUI.cs
@@ -25,15 +25,5 @@ namespace WorkSleepRepeat
             DateTime dateTime = DateTime.Now;
             _dateTime.text = $"TCR {dateTime.ToString("MM-dd")} {dateTime.ToString("HH:mm:ss")}";
         }
-
-        //private string GetFormattedDate(DateTime date)
-        //{
-        //    return string.Format("{0:mm-dd}", date.Date);
-        //}
-
-        //private string GetFormattedTime(DateTime date)
-        //{
-        //    return string.Format("{0:hh:mm:ss}", date);
-        //}
     }
 }
